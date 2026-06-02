c8a0100b48d10c7067e3e81fe9c38134b020ec45
amitakaoka <ami.rsty.0921@gmail.com>
2026-06-02 15:16:36 +0900

    Fix register page styles to match Figma

--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -9,6 +9,9 @@
 
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 
     @yield('css')
 </head>
