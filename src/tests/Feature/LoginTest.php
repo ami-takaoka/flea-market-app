b08eefcde9bea0b4d1f6144fb80e85f6ecedcec9
amitakaoka <ami.rsty.0921@gmail.com>
2026-06-08 13:21:10 +0900

    Add login feature tests

--- src/tests/Feature/LoginTest.php
+++ src/tests/Feature/LoginTest.php
@@ -3,20 +3,64 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_email_is_required(): void
+    {
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors('email');
     }
+
+    public function test_password_is_required(): void
+    {
+        $response = $this->post('/login', [
+            'email' => 'test@example.com',
+            'password' => '',
+        ]);
+
+        $response->assertSessionHasErrors('password');
+    }
+
+    public function test_user_can_login(): void
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect('/');
+
+        $this->assertAuthenticated();
+    }
+
+    public function test_user_cannot_login_with_invalid_credentials(): void
+    {
+        $response = $this->post('/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $this->assertGuest();
+
+        $response->assertSessionHasErrors([
+            'email' => trans('auth.failed'),
+        ]);
+    }
+
 }
