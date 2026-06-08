91fd16ccd956a9c24a56c113b6fdaf66f2dce83c
amitakaoka <ami.rsty.0921@gmail.com>
2026-06-08 14:44:40 +0900

    Add Logout tests

--- src/tests/Feature/LogoutTest.php
+++ src/tests/Feature/LogoutTest.php
@@ -2,21 +2,23 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LogoutTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_logout(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $this->post('/logout');
+
+        $this->assertGuest();
     }
 }
