73076fe6665b9835ca0651ae7e16011a7de39480
amitakaoka <ami.rsty.0921@gmail.com>
2026-06-09 15:12:52 +0900

    マイリスト機能のテストコードを追加

--- src/tests/Feature/ItemTest.php
+++ src/tests/Feature/ItemTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Item;
 use App\Models\User;
 use App\Models\Purchase;
+use App\Models\Like;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -74,4 +75,103 @@ class ItemTest extends TestCase
 
         $response->assertDontSee('自分の商品');
     }
+
+    public function test_mylist_displays_only_liked_items()
+    {
+        $user = User::factory()->create();
+
+        $likedItem = Item::factory()->create([
+            'name' => 'Liked Item',
+        ]);
+
+        $notLikedItem = Item::factory()->create([
+            'name' => 'Not Liked Item',
+        ]);
+
+        Like::create([
+            'user_id' => $user->id,
+            'item_id' => $likedItem->id,
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get('/?tab=mylist');
+
+        $response->assertStatus(200);
+
+        $response->assertSee('Liked Item');
+        $response->assertDontSee('Not Liked Item');
+    }
+
+    public function test_guest_cannot_see_mylist_items()
+    {
+        $item = Item::factory()->create([
+            'name' => 'Test Item',
+        ]);
+
+        $response = $this->get('/?tab=mylist');
+
+        $response->assertStatus(200);
+
+        $response->assertDontSee('Test Item');
+    }
+
+    public function test_mylist_can_be_filtered_by_keyword()
+    {
+        $user = User::factory()->create();
+
+        $watch = Item::factory()->create([
+            'name' => '腕時計',
+        ]);
+
+        $bag = Item::factory()->create([
+            'name' => 'バッグ',
+        ]);
+
+        Like::create([
+            'user_id' => $user->id,
+            'item_id' => $watch->id,
+        ]);
+
+        Like::create([
+            'user_id' => $user->id,
+            'item_id' => $bag->id,
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get('/?tab=mylist&keyword=腕');
+
+        $response->assertSee('腕時計');
+        $response->assertDontSee('バッグ');
+    }
+
+    public function test_sold_label_is_displayed_for_purchased_item_in_mylist()
+    {
+        $user = User::factory()->create();
+
+        $item = Item::factory()->create([
+            'name' => 'Sold Item',
+        ]);
+
+        Like::create([
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+        ]);
+
+        Purchase::create([
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+            'postal_code' => '123-4567',
+            'address' => '東京都渋谷区1-1-1',
+            'payment_method' => 1,
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get('/?tab=mylist');
+
+        $response->assertStatus(200);
+
+        $response->assertSee('Sold Item');
+        $response->assertSee('SOLD');
+    }
+
 }
